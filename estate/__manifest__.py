d5396022024138595b467e439ff06f7261d74cf7
Ramzi Haidan <ramzihaidan9@gmail.com>
2025-03-06 13:26:14 +0300

    adding property type

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -10,9 +10,10 @@
     'depends': ['base'],
     'data': [
         'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_property_type_view.xml',
         'views/estate_search.xml',
         'views/estate_menus.xml',
-        'views/estate_property_views.xml',
     ],
     'application': True,
     'installable': True,
