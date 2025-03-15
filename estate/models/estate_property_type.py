47fe13312bfed9065bc701bceed6cabf993301af
Ramzi Haidan <ramzihaidan9@gmail.com>
2025-03-15 23:34:04 +0300

    adding inline view for property type

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -5,3 +5,4 @@ class EstatePropertyType(models.Model):
     _description = 'Real Estate Property Type'
 
     name=fields.Char(string='Property Type')
+    property_ids = fields.One2many('estate.property','property_type_id')
